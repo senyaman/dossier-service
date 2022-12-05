08fee0f0b5e45469a0e02352043942cc515e4f61
Senya- Man <masenyagift0@gmail.com>
2022-12-05 17:46:38 +0200

    first commit

---
+++ src/main/java/com/enfint/dossierservice/DossierServiceApplication.java
@@ -0,0 +1,13 @@
+package com.enfint.dossierservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DossierServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DossierServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enfint/dossierservice/DossierServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enfint.dossierservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DossierServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
