cd9ba87853d46cb71cbc829023ad3c7057f2985b
Senya- Man <masenyagift0@gmail.com>
2022-12-07 14:24:23 +0200

    added application domain

--- src/main/java/com/enfint/dossierservice/entity/Application.java
+++ src/main/java/com/enfint/dossierservice/entity/Application.java
@@ -1,17 +1,13 @@
-package com.enfint.dealservice.entity;
+package com.enfint.dossierservice.entity;
 
-import com.enfint.dealservice.dto.ApplicationStatusHistoryDTO;
-import com.enfint.dealservice.dto.LoanOfferDTO;
-import com.enfint.dealservice.utils.ApplicationStatusEnum;
-import com.vladmihalcea.hibernate.type.json.JsonBinaryType;
+import com.enfint.dossierservice.payload.ApplicationStatusHistoryDTO;
+import com.enfint.dossierservice.payload.LoanOfferDTO;
+import com.enfint.dossierservice.utils.ApplicationStatusEnum;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.Type;
-import org.hibernate.annotations.TypeDef;
 
-import javax.persistence.*;
 import java.time.LocalDate;
 import java.util.List;
 
@@ -19,40 +15,14 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-
-@Entity
-@Table(name = "applications")
-@TypeDef(name = "jsonb", typeClass = JsonBinaryType.class)
 public class Application {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @OneToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL, orphanRemoval = true)
-    @JoinColumn(name = "client_id", referencedColumnName = "id")
     private Client client;
-
-    @OneToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL, orphanRemoval = true)
-    @JoinColumn(name = "credit_id", referencedColumnName = "id")
     private Credit credit;
-
     private ApplicationStatusEnum status;
-
-    @Column(name = "creation_date")
     private LocalDate creationDate;
-
-    @Column(name = "applied_offer", columnDefinition = "jsonb")
-    @Type(type="jsonb")
     private LoanOfferDTO appliedOffer;
-
-    @Column(name = "sign_date")
     private LocalDate signDate;
-
-    @Column(name = "ses_code")
     private Integer sesCode;
-
-    @Column(name = "status_history", columnDefinition = "jsonb")
-    @Type(type="jsonb")
     private List<ApplicationStatusHistoryDTO> statusHistory;
 }
--- src/main/java/com/enfint/dossierservice/entity/Client.java
+++ src/main/java/com/enfint/dossierservice/entity/Client.java
@@ -1,95 +1,32 @@
-package com.enfint.dealservice.entity;
+package com.enfint.dossierservice.entity;
 
-import com.enfint.dealservice.dto.EmploymentDTO;
-import com.enfint.dealservice.utils.GenderEnum;
-import com.enfint.dealservice.utils.MaritalStatusEnum;
-import com.enfint.dealservice.utils.PositionEnum;
-import com.vladmihalcea.hibernate.type.json.JsonBinaryType;
+import com.enfint.dossierservice.payload.EmploymentDTO;
+import com.enfint.dossierservice.utils.GenderEnum;
+import com.enfint.dossierservice.utils.MaritalStatusEnum;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.Type;
-import org.hibernate.annotations.TypeDef;
 
-import javax.persistence.*;
-import java.math.BigDecimal;
 import java.time.LocalDate;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-
-@Entity
-@Table(name = "clients")
-@TypeDef(name = "jsonb", typeClass = JsonBinaryType.class)
 public class Client {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @Column(name = "last_name")
     private String lastName;
-
-    @Column(name = "first_name")
     private String firstName;
-
-    @Column(name = "middle_name")
     private String middleName;
-
-    @Column(name = "birth_date")
     private LocalDate birthDate;
-
     private String email;
-
-    @Enumerated(EnumType.STRING)
     private GenderEnum gender;
-
-    @Column(name = "marital_status")
-    @Enumerated(EnumType.STRING)
     private MaritalStatusEnum maritalStatus;
-
-    @Column(name = "dependent_number")
     private Integer dependentNumber;
-
-    @Column(name = "passport_series")
     private String passportSeries;
-
-    @Column(name = "passport_number")
     private String passportNumber;
-
-    @Column(name = "issue_date")
-    private LocalDate issueDate;
-
-    @Column(name = "issue_branch")
-    private String issueBranch;
-
-    @Type(type="jsonb")
-    @Column(columnDefinition = "jsonb")
     private EmploymentDTO employment;
-
-    private String employer;
-
-    private BigDecimal salary;
-
-    @Enumerated(EnumType.STRING)
-    private PositionEnum position;
-
-    @Column(name = "work_experience_total")
-    private Integer workExperienceTotal;
-
-    @Column(name = "work_experience_current")
-    private Integer workExperienceCurrent;
-
-    @OneToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL, orphanRemoval = true)
-    @JoinColumn(name = "application_id", referencedColumnName = "id")
     private Application application;
-
-    @OneToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL, orphanRemoval = true)
-    @JoinColumn(name = "credit_id", referencedColumnName = "id")
-    private Credit credit;
-
     private String account;
 }
