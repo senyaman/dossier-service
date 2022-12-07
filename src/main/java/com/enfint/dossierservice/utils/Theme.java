ea09be95ec77c6f8464184e98b26f41a85179d53
Senya- Man <masenyagift0@gmail.com>
2022-12-07 14:15:45 +0200

    Added theme DTO

---
+++ src/main/java/com/enfint/dossierservice/utils/Theme.java
@@ -0,0 +1,10 @@
+package com.enfint.dossierservice.utils;
+
+public enum Theme {
+    FINISH_REGISTRATION,
+    CREATE_DOCUMENTS,
+    SEND_DOCUMENTS,
+    SEND_SES,
+    CREDIT_ISSUED,
+    APPLICATION_DENIED
+}
