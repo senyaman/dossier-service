64af85b479d5f5ee5e6239df2d848bb50c53cd0b
Senya- Man <masenyagift0@gmail.com>
2022-12-07 22:50:52 +0200

    finalized MailService

--- src/main/java/com/enfint/dossierservice/service/MailService.java
+++ src/main/java/com/enfint/dossierservice/service/MailService.java
@@ -1,6 +1,9 @@
 package com.enfint.dossierservice.service;
 
+import java.io.File;
+
 public interface MailService {
     void sendEmail(String to, String subject, String body);
     void sendSes(String recipient, String sesCode);
+    void sendDocument(String recipient, File file);
 }
